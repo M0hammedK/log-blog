5266357efea0bed0f6e55c35251982619a8bdb1f
M0hammedK <96379480+M0hammedK@users.noreply.github.com>
2025-02-08 14:56:55 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-<div classNane="fixed-bottom">
+<div className="fixed-bottom">
       <footer className="bg-secondary text-white py-3 d-flex justify-content-center">
         <h4>®All rights reserved</h4>
       </footer>
